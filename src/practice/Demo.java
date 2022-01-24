c1b4a5131817ce6321e4aa8b2797263053fbb199
abhis <abhis@ABHISHEK>
2022-01-25 03:02:27 +0530

    Commit Demo class & Welcome1() to branch Abhi

---
+++ src/practice/Demo.java
@@ -0,0 +1,16 @@
+package practice;
+import org.testng.annotations.Test;
+public class Demo 
+  {
+	  @Test
+	  public void Demo1()
+	    {
+		  System.out.println("Welcome to GitHub");
+	    }
+	  
+	  @Test
+	  public void Demo2()
+	    {
+	 	  System.out.println("Welcome to GitHub SDET");
+	    }
+} 
